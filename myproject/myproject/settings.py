c1df7edf4b7569309df2777e9a6c9b7e4268f932
thanhan910 <63280785+thanhan910@users.noreply.github.com>
2024-02-09 10:49:34 +1100

    Add myapp URLs and views

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'myproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'myapp/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
